aa26fb5c4ea5e1db6c7450b6c7cf57f86c18d217
Somaiyajannat <somaiyajannat044@gmail.com>
2023-09-08 01:48:35 +0600

    added adjacency list

---
+++ Algorithms/AdjacencyList/AdjacencyList/Program.cs
@@ -0,0 +1,22 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Adjacency List");
+        // Adjacency list declaration
+
+        Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
+        graph[0] = new List<int> {1, 2};
+        graph[1] = new List<int> {0, 2};
+        graph[2] = new List<int> {0, 1};
+
+        Dictionary<char, List<char>> graph2 = new Dictionary<char, List<char>>();
+
+        graph2['A'] = new List<char> { 'B','C'};
+        graph2['B'] = new List<char> { 'A', 'C'};
+        graph2['C'] = new List<char> { 'B', 'D' };
+        graph2['D'] = new List<char> { 'A', 'C' };
+
+            
+    }
+}
