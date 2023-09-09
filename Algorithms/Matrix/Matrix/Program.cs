ab805bfc68c61231ea5e10bfba6d6987cd805551
Somaiyajannat <somaiyajannat044@gmail.com>
2023-09-09 23:44:56 +0600

    Multi dimen

---
+++ Algorithms/Matrix/Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,] graph = new int[2, 3];
+        int[,] graph1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+        // taking input from user for graph 
+
+        for(int i = 0; i < 2; i++)
+        {
+            for(int j = 0; j< 3; j++)
+            {
+                var value = Convert.ToInt32(Console.ReadLine());
+                graph[i, j] = value;
+            }
+            
+        }
+        // print graph
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write(graph[i, j] + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+
+    }
+}
